dfcb118da2f2c09abef00ef33cc41da56c999795
S Lee <sanlee8495@gmail.com>
2022-07-24 13:38:26 -0700

    esquisse with shiny from Busines Science r tips (Matt Dancho)
    7/24/22

--- Library - Shiny.R
+++ Library - Shiny.R
@@ -153,3 +153,88 @@ server <- function(input, output, session){
 
 
 
+###############################################################################################################
+########################################### shiny with esquisse ###############################################
+###############################################################################################################
+
+# all you need to is to change the data
+
+library(esquisse)
+library(modeldata)
+library(shiny)
+
+data("drinks")
+data("mpg")
+
+drinks
+mpg
+
+ui <- fluidPage(
+  
+  titlePanel("Use esquisse as a Shiny module"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(
+        inputId = "data",
+        label = "Data to use:",
+        choices = c("drinks", "mpg"),
+        inline = TRUE
+      )
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel(
+          title = "esquisse",
+          esquisserUI(
+            id = "esquisse",
+            header = FALSE, # dont display gadget title
+            choose_data = FALSE # dont display button to change data
+          )
+        ),
+        tabPanel(
+          title = "output",
+          verbatimTextOutput("module_out")
+        )
+      )
+    )
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  data_r <- reactiveValues(data = drinks, name = "drinks")
+  
+  observeEvent(input$data, {
+    if (input$data == "drinks") {
+      data_r$data <- drinks
+      data_r$name <- "drinks"
+    } else {
+      data_r$data <- mpg
+      data_r$name <- "mpg"
+    }
+  })
+  
+  result <- callModule(
+    module = esquisserServer,
+    id = "esquisse",
+    data = data_r
+  )
+  
+  output$module_out <- renderPrint({
+    str(reactiveValuesToList(result))
+  })
+  
+}
+
+shinyApp(ui, server)
+
+
+
+
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+
+
